e3aafa06c1f831abdb88c7fc697fe4100cb70bdc
SriPadma8997-egov <sri.padma@egovernments.org>
2025-09-11 15:01:22 +0530

    Added hcm-demo-latest terraform configuration

---
+++ infra-as-code/terraform/hcm-demo-latest-aws/remote-state/variables.tf
@@ -0,0 +1,3 @@
+variable "bucket_name" {
+  default = "hcm-demo-latest-s3-state-bckt"
+}
---
+++ infra-as-code/terraform/hcm-demo-latest-aws/variables.tf
@@ -0,0 +1,66 @@
+#
+# Variables Configuration. Check for REPLACE to substitute custom values. Check the description of each
+# tag for more information
+#
+
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
+  default = "hcm-demo-latest" #REPLACE
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block"
+  default = "192.168.0.0/16"
+}
+
+
+variable "network_availability_zones" {
+  description = "Configure availability zones configuration for VPC. Leave as default for India. Recommendation is to have subnets in at least two availability zones"
+  default = ["ap-south-1a", "ap-south-1b"] #REPLACE IF NEEDED
+}
+
+variable "availability_zones" {
+  description = "Amazon EKS runs and scales the Kubernetes control plane across multiple AWS Availability Zones to ensure high availability. Specify a comma separated list to have a cluster spanning multiple zones. Note that this will have cost implications"
+  default = ["ap-south-1b"] #REPLACE IF NEEDED
+}
+
+variable "kubernetes_version" {
+  description = "kubernetes version"
+  default = "1.31"
+}
+
+variable "instance_types" {
+  description = "Arry of instance types for SPOT instances"
+  default = ["r6a.large"]
+  
+}
+
+variable "min_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for min nodes"
+  default = "1" #REPLACE IF NEEDED
+}
+
+variable "desired_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for desired nodes"
+  default = "5" #REPLACE IF NEEDED
+}
+
+variable "max_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for max nodes"
+  default = "5" #REPLACE IF NEEDED
+}
+
+variable "ami_id" {
+  description = "Provide the AMI ID that supports your eks version"
+  default = {
+    id   = "ami-0d1008f82aca87cb9"
+    name = "amazon-eks-node-1.30-v20241024"
+  }
+}
+
+variable "enable_karpenter" {
+  description = "Enable the karpenter."
+  type        = bool
+  default     = false
+}
+
